1cebc7eec3230b32b0067bfa1c968abe3884ed9f
madame3369 <akeka0303@naver.com>
2024-07-24 22:25:58 +0900

    feat: dockerfile 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 import userRoutes from './routes/userRoutes';
 import teamRoutes from './routes/teamRoutes';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+// const HOST = process.env.HOST || 'localhost';
+
+// const corsOptions = {
+//     origin: ['http://localhost:3000', 'http://captaincheoni.com'],
+//     optionsSuccessStatus: 200
+//   };
+  
+// app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -11,5 +22,5 @@ app.use('/api/users', userRoutes);
 app.use('/api/teams', teamRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
